dfb3e6b023eaffef9e27c91d948c1b0dcbcdb0ec
Shalini Gogikar <shalinigogikar23@gmail.com>
2025-01-20 21:59:07 +0530

    Added MealsSummary

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Fragment } from "react";
-import Header from "./components/Layout/Header"
+import Header from "./components/Layout/Header";
+import MealsSummary from "./components/Meals/MealsSummary";
 function App() {
   return (
     <Fragment>
       <Header/>
+      <MealsSummary/>
     </Fragment>
   );
 }
