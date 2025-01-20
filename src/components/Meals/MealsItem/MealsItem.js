0c04bbfed0ea89cef4461e0af0ef634189b50ed1
Shalini Gogikar <shalinigogikar23@gmail.com>
2025-01-20 23:05:44 +0530

    meals array is updated

---
+++ src/components/Meals/MealsItem/MealsItem.js
@@ -0,0 +1,12 @@
+import classes from "./MealsItem.module.css";
+const MealsItem=props=>{
+    const price='$${props.price.toFixed(2)';
+return<li>
+    <div className={classes.meal}>
+        <h3>{props.name}</h3>
+        <div className={classes.description}>{props.description}</div>
+        <div className={classes.price}>{props.price}</div>
+    </div>
+</li>
+};
+export default MealsItem;
