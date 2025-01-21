ec7c8325fb20eb7e31bd4d198ff4003e093fc142
Shalini Gogikar <shalinigogikar23@gmail.com>
2025-01-21 20:43:53 +0530

    Updated AddForm

--- src/components/Meals/MealsItem/MealsItem.js
+++ src/components/Meals/MealsItem/MealsItem.js
@@ -1,12 +1,18 @@
 import classes from "./MealsItem.module.css";
+import MealItemForm from "./MealItemForm"
 const MealsItem=props=>{
     const price='$${props.price.toFixed(2)';
-return<li>
+return(
+<li>
     <div className={classes.meal}>
         <h3>{props.name}</h3>
         <div className={classes.description}>{props.description}</div>
         <div className={classes.price}>{props.price}</div>
+    <div>
+        <MealItemForm/>
+    </div>
     </div>
 </li>
+);
 };
 export default MealsItem;
