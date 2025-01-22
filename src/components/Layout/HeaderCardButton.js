b9ac073012b23ae9a1c815b0c85a3e63abef1ac9
Shalini Gogikar <shalinigogikar23@gmail.com>
2025-01-22 20:48:19 +0530

    Making a Hard coded cart used React.createportal()

--- src/components/Layout/HeaderCardButton.js
+++ src/components/Layout/HeaderCardButton.js
@@ -1,9 +1,9 @@
-import CardIcon from "../Card/CardIcon";
+import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCardButton.module.css";
 const HeaderCardButton =props=>
 {
 return<button className={classes.button}>
-    <span className={classes.icon}><CardIcon /></span>
+    <span className={classes.icon}><CartIcon /></span>
     <span>your Cart</span>
     <span className={classes.badge}>0</span>
 </button>
