2606313ed88826e16d5afe7daa59cd45e216eaf0
Shalini Gogikar <shalinigogikar23@gmail.com>
2025-01-23 10:30:44 +0530

    updated close button

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -14,7 +14,7 @@ const cartItems=(
             <span>35.62</span>
         </div>
         <div className={classes.actions}>
-        <button className={classes['button--alt']}>Close</button>
+        <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
         <button className={classes.button}>Order</button>
         </div></Modal>
     );
