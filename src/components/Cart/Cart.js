ceee6a95513bc6ceb541611e49e87b799687230a
Shalini Gogikar <shalinigogikar23@gmail.com>
2025-01-26 00:31:46 +0530

    Item order is displaced

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,17 +1,27 @@
+import React,{useContext} from "react";
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
+import CartContext from "../../store/cart-context";
 const Cart=(props)=>{
-const cartItems=(
-    <ul className={classes['cart-items']}>
-    {[{id:'c1', name:'Sushi', amount:2 ,price: 12.99}].map((item)=>
-        (<li>{item.name}</li>))}
+    const cartCxt=useContext(CartContext);
+    console.log(cartCxt.items);
+const cartItems=(<ul className={classes['cart-items']}>
+    {cartCxt.items.map((item,index)=>
+        (<li key={index}>Name:{item.name} Price:{item.price} Quantity: {item.quantity}</li>))}
 </ul>);
+let total=0;
+cartCxt.items.forEach((item)=>
+    {
+        const price = Number(item.price) || 0; // Ensure price is a valid number, default to 0
+    const quantity = Number(item.quantity) || 0;
+       total+=price*quantity;
+    });
     return(
         <Modal onClose={props.onClose}>
             {cartItems}
             <div className={classes.total}>
             <span>Total Amount:</span>
-            <span>35.62</span>
+            <span>{total}</span>
         </div>
         <div className={classes.actions}>
         <button className={classes['button--alt']} onClick={props.onClose}>Close</button>
